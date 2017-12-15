7c8ddafaebf26a46e3c82217dc2e72ef9e2cd953
ChenqXu <cx2198@columbia.edu>
2017-12-15 00:15:09 -0500

    init

--- application.py
+++ application.py
@@ -2,17 +2,93 @@ import os
 from flask import Flask, request, render_template, session, redirect
 from flask_cors import CORS
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'template')
 application = Flask(__name__, template_folder=tmpl_dir)
 CORS(application)
 
 application.secret_key = 'super secret key'
 application.config['SESSION_TYPE'] = 'filesystem'
 
+
+def args2dict(request_args, args):
+    res = {}
+    for arg in args:
+        res[arg] = request_args.form[arg]
+    return res
+
+
 @application.route('/')
 def home_page():
     return render_template('login.html')
 
+
+@application.route('/register/')
+def register_page():
+    return render_template('register.html')
+
+
+@application.route('/student/create/', methods=['POST'])
+def register_student():
+    args = ["nick_name", "avatar", "email", "password",
+            "introduction"]
+    student = args2dict(request, args)
+    # new_student = create_student_l(student)
+    # session['user'] = new_student
+    return redirect('/student/')
+
+
+@application.route('/student/')
+def user_home_page():
+    if session['user'] is not None:
+        return render_template('user.html')
+
+    else:
+        return render_template('error.html')
+
+
+@application.route('/student/login/', methods=['POST'])
+def login_student():
+    student_id = request.form['student_id']
+    password = request.form['password']
+    # student = login(student_id, password)
+    student = {'user_id': 1, 'nick_name': 'jack',
+               'avatar': 'https://pbs.twimg.com/profile_images/747403736293617664/5pPvHX0G_400x400.jpg',
+               'email': 'russwest44@gmail.com', 'password': '123456', 'introduction': 'why'}
+    if student is not None:
+        session['user'] = student
+        return redirect('/student/')
+
+    else:
+        return render_template('error.html')
+
+
+@application.route('/discover/', methods=['GET'])
+def get_all_moment():
+    user_id = session['user']['user_id']
+    # moments = get_my_moment(user_id)
+    # context = dict(moments=moments)
+    context = [{'nick_name': 'jack', 'time': '2017-12-20',
+               'image': 'https://i.ytimg.com/vi/zNCz4mQzfEI/maxresdefault.jpg', 'content': 'I would like to see coco.'}]
+    return render_template('discover.html', events=context)
+
+'''
+@application.route('/trend/', methods=['GET'])
+def my_trend():
+    user_id = session['user']['user_id']
+    trends = get_my_trend_l(user_id)
+    my_circles = get_my_circle(user_id)
+    context = dict(trends=trends, circles=my_circles)
+    return render_template('trend.html', **context)
+
+
+@application.route('/circle/', methods=['GET'])
+def get_circle_school():
+    user_id = session['user']['user_id']
+    school_id = session['user']['school_id']
+    circles = get_all_circle_l(user_id, school_id)
+    context = dict(circles=circles)
+    return render_template('circle.html', **context)
+'''
 
 if __name__ == '__main__':
     import click
