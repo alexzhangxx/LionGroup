4608d1ca9b7afa832a8b77a4784d1c891040aa81
ChenqXu <cx2198@columbia.edu>
2017-12-14 19:46:04 -0500

    init

--- application.py
+++ application.py
@@ -1,1 +1,39 @@
+import os
+from flask import Flask, request, render_template, session, redirect
+from flask_cors import CORS
 
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+application = Flask(__name__, template_folder=tmpl_dir)
+CORS(application)
+
+application.secret_key = 'super secret key'
+application.config['SESSION_TYPE'] = 'filesystem'
+
+@application.route('/')
+def home_page():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    import click
+
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
+        This function handles command line parameters.
+        Run the server using:
+            python server.py
+        Show the help text using:
+            python server.py --help
+        """
+
+        HOST, PORT = host, port
+        application.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+    run()
